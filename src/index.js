f0fc38f87d935ee5c3991084a2640e1f32ba4828
Gassan <g_fox_fts@yahoo.com>
2020-07-29 01:19:21 +0200

    ScriptExtHtmlWebpackPlugin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app');
 
-app.innerHTML = ``;
+app.innerHTML = `<h1>Javascript DOM</h1>`;
 
+/* document.addEventListener('DomContentloaded',()=>{
+    alert('DomContentloaded')
+})
+window.addEventListener('load',()=>alert('load'))
+*/
 
