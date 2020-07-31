0e77dc94691d92362c33f09b9c20e47f7dbfe18b
Gassan <g_fox_fts@yahoo.com>
2020-07-31 16:49:22 +0200

    document.create

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import '../assets/css/style.css';
 
-const app = document.getElementById('app');
 
-app.innerHTML = `<h1>Javascript DOM</h1>`;
+// app.innerHTML = `<h1>Javascript DOM</h1>`;
 
 /* document.addEventListener('DomContentloaded',()=>{
     alert('DomContentloaded')
 })
 window.addEventListener('load',()=>alert('load'))
 */
-console.log(document.documentElement)
+/* console.log(document.documentElement)
 console.dir(document.documentElement)
 
 console.log(document.head)
 console.dir(document.body)
 console.log(document.body.constructor.name)
 console.log(document.body)
+ */
+const app = document.getElementById('app');
+
+const div = document.createElement('div')
+const text = document.createTextNode('DOM!')
+const commet = document.createComment('No commet')
+app.append(div)
+app.append(commet)
+
+div.append(text)
+
+console.log(app,div,text)
+
