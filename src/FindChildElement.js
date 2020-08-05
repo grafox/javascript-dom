829868eecaf20b3dac0f082dde7dc5c53e557349
Gassan <g_fox_fts@yahoo.com>
2020-08-05 17:20:32 +0200

    FindChildElement

---
+++ src/FindChildElement.js
@@ -0,0 +1,11 @@
+import '../assets/css/style.css';
+
+const app = document.getElementById('app')
+app.innerHTML = `<h1>Javascript DOM</h1>
+<ul id="list">
+    <li>Earth</li>
+    <li>Fire</li>
+    <li>Water</li>
+    <li>Air</li>
+</ul>
+`
--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import '../assets/css/style.css';
 
 const app = document.getElementById('app')
 app.innerHTML = `<h1>Javascript DOM</h1>
-
+<ul id="list">
+    <li>Earth</li>
+    <li>Fire</li>
+    <li>Water</li>
+    <li>Air</li>
+</ul>
 `
+const list = document.querySelector('#list')
+const selectedIndex = 2
+//querySelectorAll
+const queryChildern = list.querySelectorAll('li')
+console.log(queryChildern,queryChildern.length)
+//.children: HTMLCollection
+console.log(list.children);
+//.childNodes: NodeList
+console.log(list.childNodes);
+
+console.log(list.children[selectedIndex]);
