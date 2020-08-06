0ad2e102206e7c938362c76fee0dd933f4c27df3
Gassan <g_fox_fts@yahoo.com>
2020-08-07 00:33:30 +0200

    PreventingDefaultEventActions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import '../assets/css/style.css';
 const app = document.getElementById('app')
 app.innerHTML = `<h1>Javascript DOM</h1>
 <div id="app"></div>
-<button type="button" class="One Two">
-    Click me!
-</button>
+    <div class="one">
+        <div class="two">
+            <button type="button" class="three">
+                Click Me
+            </button>
+        </div>
+    </div>
 `
