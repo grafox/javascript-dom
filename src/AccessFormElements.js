f0b9774c8e4d911a9c4553676c4096a250c5ae9f
Gassan <g_fox_fts@yahoo.com>
2020-08-09 22:05:00 +0200

    accessing form Elements

--- src/AccessFormElements.js
+++ src/AccessFormElements.js
@@ -11,3 +11,15 @@ app.innerHTML = `<h1>Javascript DOM</h1>
         </label>
     </form>
 `
+const form = document.forms.order
+const {fullname,email} = form.elements
+
+function handleInput(event){
+    // access the value
+    console.log(event.target.value)
+    // access the form
+    console.log(event.target.form);
+}
+
+fullname.addEventListener('input',handleInput)
+email.addEventListener('change',handleInput)
